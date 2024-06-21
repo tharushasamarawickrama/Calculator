8e8fc56a20d5070f638536d80a587046ed528b74
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-06-21 14:30:52 +0530

    make a simple calcularor

---
+++ script.js
@@ -0,0 +1,22 @@
+//calculator program
+
+const display= document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch(error){
+        display.value="Error";
+    }
+    
+}
+
+function clearDisplay(){
+    display.value = "";
+}
